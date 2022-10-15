f83642d685981d7c19109a240917274745d3d7e1
Windula Dissanayake <charithwinwindula@gmail.com>
2022-10-15 12:11:23 +0530

    add files in local machine

---
+++ JS_Functions/func_1/func_1.js
@@ -0,0 +1,4 @@
+function func_1()
+{
+    alert("Hello world!");
+}
---
+++ exe_13/function.js
@@ -0,0 +1,27 @@
+function func_add(a, b)
+{
+    num1 = document.getElementById("input_1").value;
+    num2 = document.getElementById("input_2").value;
+    document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function func_sub(a, b)
+{
+    num1 = document.getElementById("input_1").value;
+    num2 = document.getElementById("input_2").value;
+    document.getElementById("result").innerHTML = num1 - num2;
+}
+
+function func_mul(a, b)
+{
+    num1 = document.getElementById("input_1").value;
+    num2 = document.getElementById("input_2").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function func_div(a, b)
+{
+    num1 = document.getElementById("input_1").value;
+    num2 = document.getElementById("input_2").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+}
